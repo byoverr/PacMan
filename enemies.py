bdbfe07cacc19c0b6e81641c8f4e3ecde37f2c5f
Unkleverdly <83646403+Unkleverdly@users.noreply.github.com>
2022-01-23 10:01:14 +0300

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -5,43 +5,43 @@ SCREEN_WIDTH = 475
 SCREEN_HEIGHT = 625
 
 # Define some colors
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-BLUE = (0,0,255)
-GREEN = (0,255,0)
-RED = (255,0,0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self,x,y,color,width,height):
+    def __init__(self, x, y, color, width, height):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([width,height])
-        self.image.fill(BLUE)
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.topleft = (x,y)
+        self.rect.topleft = (x, y)
 
 
 class Ellipse(pygame.sprite.Sprite):
-    def __init__(self,x,y,color,width,height):
+    def __init__(self, x, y, color, width, height):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([width,height])
+        self.image = pygame.Surface([width, height])
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
-        pygame.draw.ellipse(self.image,color,[0,0,width,height])
+        pygame.draw.ellipse(self.image, color, [0, 0, width, height])
         self.rect = self.image.get_rect()
-        self.rect.topleft = (x,y)
+        self.rect.topleft = (x, y)
 
-        
+
 class Slime(pygame.sprite.Sprite):
-    def __init__(self,x,y,change_x,change_y):
+    def __init__(self, x, y, change_x, change_y):
         pygame.sprite.Sprite.__init__(self)
         self.change_x = change_x
         self.change_y = change_y
         self.image = pygame.image.load("slime.png").convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.topleft = (x,y)
- 
+        self.rect.topleft = (x, y)
 
-    def update(self,horizontal_blocks,vertical_blocks):
+    def update(self, horizontal_blocks, vertical_blocks):
         self.rect.x += self.change_x
         self.rect.y += self.change_y
         if self.rect.right < 0:
@@ -54,7 +54,7 @@ class Slime(pygame.sprite.Sprite):
             self.rect.bottom = 0
 
         if self.rect.topleft in self.get_intersection_position():
-            direction = random.choice(("left","right","up","down"))
+            direction = random.choice(("left", "right", "up", "down"))
             if direction == "left" and self.change_x == 0:
                 self.change_x = -2
                 self.change_y = 0
@@ -67,50 +67,49 @@ class Slime(pygame.sprite.Sprite):
             elif direction == "down" and self.change_y == 0:
                 self.change_x = 0
                 self.change_y = 2
-                
 
     def get_intersection_position(self):
         items = []
-        for i,row in enumerate(enviroment()):
-            for j,item in enumerate(row):
-                if item == 3:
-                    items.append((j*25,i*25))
+        for i, row in enumerate(enviroment()):
+            for j, item in enumerate(row):
+                if item == 19:
+                    items.append((j * 25, i * 25))
 
         return items
-    
-        
+
+
 def enviroment():
-    grid = ((0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0),
-            (0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0),
-            (2,   10,  10,  10,  10,  10,  10,  10,  10,  12,  10,  10,  10,  10,  10,  10,  10,  10,  3),
-            (11,  0,   1,   1,   1,   1,   1,   1,   1,   11,  1,   1,   1,   1,   1,   1,   1,   1,   11),
-            (11,  16,  2,   3,   16,  2,   12,  3,   16,  11,  1,   2,   12,  3,   1,   2,   3,   1,   11),
-            (11,  16,  4,   5,   16,  4,   13,  5,   16,  9,   1,   4,   13,  5,   1,   4,   5,   1,   11),
-            (11,  16,  1,   1,   16,  1,   1,   1,   16,  1,   1,   1,   1,   1,   1,   1,   1,   1,   11),
-            (11,  16,  6,   7,   16,  8,   1,   6,   10,  12,  10,  7,   1,   8,   1,   6,   7,   1,   11),
-            (11,  16,  1,   1,   16,  11,  1,   1,   1,   11,  1,   1,   1,   11,  1,   1,   1,   1,   11),
-            (4,   10,  10,  3,   1,   14,  10,  7,   1,   9,   1,   6,   10,  15,  1,   2,   10,  10,  5),
-            (0,   0,   0,   11,  1,   11,  1,   1,   1,   1,   1,   1,   1,   11,  1,   11,  0,   0,   0),
-            (10,  10,  10,  5,   1,   9,   1,   2,   12,  12,  12,  3,   1,   9,   1,   4,   10,  10,  10),
-            (1,   1,   1,   1,   1,   1,   1,   14,  0,   0,   0,   15,  1,   1,   1,   1,   1,   1,   1),
-            (10,  10,  10,  3,   1,   8,   1,   4,   13,  13,  13,  5,   1,   8,   1,   2,   10,  10,  10),
-            (0,   0,   0,   11,  1,   11,  1,   1,   1,   1,   1,   1,   1,   11,  1,   11,  0,   0,   0),
-            (2,   10,  10,  5,   1,   9,   1,   6,   10,  12,  10,  7,   1,   9,   1,   4,   10,  10,  3),
-            (11,  0,   1,   1,   1,   1,   1,   1,   1,   11,  1,   1,   1,   1,   1,   1,   1,   1,   11),
-            (11,  1,   6,   3,   1,   6,   10,  7,   1,   9,   1,   6,   10,  7,   1,   2,   7,   1,   11),
-            (11,  1,   1,   11,  1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   11,  1,   1,   11),
-            (14,  7,   1,   9,   1,   8,   1,   6,   10,  12,  10,  7,   1,   8,   1,   9,   1,   6,   15),
-            (11,  1,   1,   1,   1,   11,  1,   1,   1,   11,  1,   1,   1,   11,  1,   1,   1,   1,   11),
-            (11,  1,   6,   10,  10,  13,  10,  7,   1,   9,   1,   6,   10,  13,  10,  10,  7,   1,   11),
-            (11,  0,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   11),
-            (4,   10,  10,  10,  10,  10,  10,  10,  10,  12,  10,  10,  10,  10,  10,  10,  10,  10,  5))
+    grid = ((0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            (2, 10, 10, 10, 10, 10, 10, 10, 10, 12, 10, 10, 10, 10, 10, 10, 10, 10, 3),
+            (11, 19, 1, 1, 19, 1, 1, 1, 19, 11, 19, 1, 1, 1, 19, 1, 1, 19, 11),
+            (11, 16, 2, 3, 16, 2, 12, 3, 16, 11, 16, 2, 12, 3, 16, 2, 3, 16, 11),
+            (11, 16, 4, 5, 16, 4, 13, 5, 16, 9, 16, 4, 13, 5, 16, 4, 5, 16, 11),
+            (11, 19, 1, 1, 19, 1, 19, 1, 19, 1, 19, 1, 19, 1, 19, 1, 1, 19, 11),
+            (11, 16, 6, 7, 16, 8, 16, 6, 10, 12, 10, 7, 16, 8, 16, 6, 7, 16, 11),
+            (11, 19, 1, 1, 19, 11, 19, 1, 19, 11, 19, 1, 19, 11, 19, 1, 1, 19, 11),
+            (4, 10, 10, 3, 16, 14, 10, 7, 16, 9, 16, 6, 10, 15, 16, 2, 10, 10, 5),
+            (0, 0, 0, 11, 16, 11, 19, 1, 19, 1, 19, 1, 19, 11, 16, 11, 0, 0, 0),
+            (10, 10, 10, 5, 16, 9, 16, 2, 12, 12, 12, 3, 16, 9, 16, 4, 10, 10, 10),
+            (1, 1, 1, 1, 19, 1, 19, 14, 0, 0, 0, 15, 19, 1, 19, 1, 1, 1, 1),
+            (10, 10, 10, 3, 16, 8, 16, 4, 13, 13, 13, 5, 16, 8, 16, 2, 10, 10, 10),
+            (0, 0, 0, 11, 16, 11, 19, 1, 1, 1, 1, 1, 19, 11, 16, 11, 0, 0, 0),
+            (2, 10, 10, 5, 16, 9, 16, 6, 10, 12, 10, 7, 16, 9, 16, 4, 10, 10, 3),
+            (11, 19, 1, 1, 19, 1, 19, 1, 19, 11, 19, 1, 19, 1, 19, 1, 1, 19, 11),
+            (11, 16, 6, 3, 16, 6, 10, 7, 16, 9, 16, 6, 10, 7, 16, 2, 7, 16, 11),
+            (11, 19, 19, 11, 19, 1, 19, 1, 19, 20, 19, 1, 19, 1, 19, 11, 19, 19, 11),
+            (14, 7, 16, 9, 16, 8, 16, 6, 10, 12, 10, 7, 16, 8, 16, 9, 16, 6, 15),
+            (11, 19, 19, 1, 19, 11, 19, 1, 19, 11, 19, 1, 19, 11, 19, 1, 19, 19, 11),
+            (11, 16, 6, 10, 10, 13, 10, 7, 16, 9, 16, 6, 10, 13, 10, 10, 7, 16, 11),
+            (11, 19, 1, 1, 1, 1, 1, 1, 19, 1, 19, 1, 1, 1, 1, 1, 1, 19, 11),
+            (4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 5))
 
     return grid
 
 
 def draw_enviroment(screen):
-    for i,row in enumerate(enviroment()):
-        for j,item in enumerate(row):
+    for i, row in enumerate(enviroment()):
+        for j, item in enumerate(row):
             if item == 2:
                 pygame.draw.line(screen, BLUE, [j * 25, i * 25], [j * 25 + 25, i * 25], 2)
                 pygame.draw.line(screen, BLUE, [j * 25, i * 25], [j * 25, i * 25 + 25], 2)
@@ -141,16 +140,15 @@ def draw_enviroment(screen):
                 pygame.draw.line(screen, BLUE, [j * 25, i * 25 + 25], [j * 25 + 25, i * 25 + 25], 2)
             elif item == 10:
                 pygame.draw.line(screen, BLUE, [j * 25, i * 25], [j * 25 + 25, i * 25], 2)
-                pygame.draw.line(screen, BLUE, [j * 25, i * 25 + 25], [j * 25 + 25, i*25 + 25], 2)
+                pygame.draw.line(screen, BLUE, [j * 25, i * 25 + 25], [j * 25 + 25, i * 25 + 25], 2)
             elif item == 11:
                 pygame.draw.line(screen, BLUE, [j * 25, i * 25], [j * 25, i * 25 + 25], 2)
                 pygame.draw.line(screen, BLUE, [j * 25 + 25, i * 25], [j * 25 + 25, i * 25 + 25], 2)
             elif item == 12:
-                pygame.draw.line(screen, BLUE, [j * 25, i * 25], [j * 25+25, i*25], 2)
+                pygame.draw.line(screen, BLUE, [j * 25, i * 25], [j * 25 + 25, i * 25], 2)
             elif item == 13:
                 pygame.draw.line(screen, BLUE, [j * 25, i * 25 + 25], [j * 25 + 25, i * 25 + 25], 2)
             elif item == 14:
                 pygame.draw.line(screen, BLUE, [j * 25, i * 25], [j * 25, i * 25 + 25], 2)
             elif item == 15:
                 pygame.draw.line(screen, BLUE, [j * 25 + 25, i * 25], [j * 25 + 25, i * 25 + 25], 2)
-
