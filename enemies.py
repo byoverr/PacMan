d1f1c2bfd2fac2c5ee814f2743e5b221594e3bf3
Unkleverdly <83646403+Unkleverdly@users.noreply.github.com>
2022-01-31 13:00:47 +0300

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -1,15 +1,6 @@
 import pygame
 import random
-
-SCREEN_WIDTH = 475
-SCREEN_HEIGHT = 625
-
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+from characteristic import *
 
 
 class Block(pygame.sprite.Sprite):
